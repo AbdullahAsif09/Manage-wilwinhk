5a3f4206a2a286b9a075d8c8780e8b74554d915e
Umair-Ali-01 <umairsadaqat@invotyx.co.uk>
2023-06-17 15:57:47 +0500

    Formatting

--- controllers/forgetPassword.js
+++ controllers/forgetPassword.js
@@ -1,32 +1,31 @@
-const User = require('../models/User');
-const randomString = require('randomstring')
-const resetPasswordMail = require('../mails/resetPasswordMail')
-
+const User = require("../models/User");
+const randomString = require("randomstring");
+const resetPasswordMail = require("../mails/resetPasswordMail");
 
 const forgetPassword = async (req, res) => {
-    try {
-        const email = req.body.email
-        const userData = await User.findOne({ email: email });
-        if (userData) {
-            if (userData.is_verified === 0) {
-                return res.status(300).send({ message: "Please verify your mail..." })
-            }
-            else {
-                const rString = randomString.generate();
-                const data = await User.updateOne({ email: email }, { $set: { token: rString } })
-                resetPasswordMail(userData.name, userData.email, rString)
-                res.status(200).send({ success: true, msg: "Please check your mail..." })
-            }
-
-
-        }
-        else {
-            res.status(400).send({ success: false, msg: "Email not found..." })
-        }
-    } catch (error) {
-        res.status(400).send({ success: false, msg: error.message });
-
+  try {
+    const email = req.body.email;
+    const userData = await User.findOne({ email: email });
+    if (userData) {
+      if (userData.is_verified === 0) {
+        return res.status(300).send({ message: "Please verify your mail..." });
+      } else {
+        const rString = randomString.generate();
+        const data = await User.updateOne(
+          { email: email },
+          { $set: { token: rString } }
+        );
+        resetPasswordMail(userData.name, userData.email, rString);
+        res
+          .status(200)
+          .send({ success: true, msg: "Please check your mail..." });
+      }
+    } else {
+      res.status(400).send({ success: false, msg: "Email not found..." });
     }
-}
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error.message });
+  }
+};
 
-module.exports = { forgetPassword }
+module.exports = { forgetPassword };
