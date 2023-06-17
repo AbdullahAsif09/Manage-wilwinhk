51ee05ace160c95607a70daf4a45f498ad3b707e
Umair-Ali-01 <umairsadaqat@invotyx.co.uk>
2023-06-17 20:05:58 +0500

    VerifyMail JWT Token [Api]

--- mails/verifymail.js
+++ mails/verifymail.js
@@ -1,12 +1,22 @@
 const User = require("../models/User");
+const JWT = require("jsonwebtoken");
 
 const verifyMail = async (req, res) => {
   try {
-    const verify = await User.updateOne(
-      { _id: req.query.id },
-      { $set: { is_verified: 1, new: true } }
-    );
-
+    const authToken = req.query.token; // incripted token
+    const decode = JWT.verify(authToken, process.env.JWT_KEY); // decrypt token
+    const id = decode.user.id;
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).send({ error: "User not found" });
+      return;
+    }
+    if (user.is_verified) {
+      res.status(400).send({ message: "Email already verified..." });
+      return;
+    }
+    user.is_verified = 1;
+    await user.save();
     res.status(200).send({ message: "Email verified successfully..." });
 
     console.log(verify);
